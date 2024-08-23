47e7c83f72fd4d14c7feb7d873b86daed1b67587
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-23 23:42:08 +0700

    fix bug no return in App Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,22 +9,25 @@ import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/collection" element={<Collection />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/product/:productId" element={<Product />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/place-order" element={<PlaceOrder />} />
-      <Route path="/orders" element={<Orders />} />
-    </Routes>
-    ;
-  </div>;
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
