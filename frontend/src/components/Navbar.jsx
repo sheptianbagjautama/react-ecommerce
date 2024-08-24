02b54bb08ec6bc3815082fff2ec33db3689997ef
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-24 18:27:54 +0700

    Navbar link mobile active and route logo into home page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,9 @@ function Navbar() {
   const [visible, setVisible] = useState(false);
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} className="w-36" />
+      <Link to="/">
+        <img src={assets.logo} className="w-36" />
+      </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
