9fdea7e613dca1efaa14b8cf281ca1837ccf72ea
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-09-20 23:55:02 +0700

    Add Routes, Route in App Component
    add Route /add, /list, /orders

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,9 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import Sidebar from "./components/Sidebar.jsx";
+import Add from "./pages/Add.jsx";
+import List from "./pages/List.jsx";
+import Orders from "./pages/Orders.jsx";
 
 const App = () => {
   return (
@@ -9,6 +13,13 @@ const App = () => {
         <hr />
         <div className="flex w-full">
           <Sidebar />
+          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base ">
+            <Routes>
+              <Route path="/add" element={<Add />} />
+              <Route path="/list" element={<List />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
         </div>
       </>
     </div>
